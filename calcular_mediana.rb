2cf946c1148a4df2c5c18b5b73aa0916ee4e73ca
Gabriel Rondon <gabrielbarragan07@gmail.com>
2022-05-23 21:23:16 -0500

    calcular mediana terminado

---
+++ calcular_mediana.rb
@@ -0,0 +1,19 @@
+if __FILE__ == $PROGRAM_NAME
+
+    def median(lista)
+        midd = lista.length / 2
+
+        lista.length % 2 == 0 ? mediana = (lista[midd] + lista[midd -1 ]) / 2.0 : mediana = lista[midd]
+      
+
+        return mediana
+    end
+
+
+    p median([4, 5, 6]) == 5
+    p median([-3, 0, 3]) == 0
+    p median([2, 3, 4, 5]) == 3.5
+    p median([1, 8, 10]) == 8
+
+
+end
